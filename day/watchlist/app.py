8a12b20774c9630577726972fe1882c157df1415
Zuiru <2425277916@qq.com>
2020-04-02 18:16:21 +0800

    模板

--- day/watchlist/app.py
+++ day/watchlist/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask,url_for,render_template
 app = Flask(__name__)
 
-@app.route('/user/<name>')
+@app.route('/')
+def index():
+    name = "Dom"
+    movies = [
+        {'title':"大赢家","year":"2020"},
+        {'title':"主女","year":"2020"},
+        {'title':"狗女","year":"2020"},
+        {'title':"猫女","year":"2020"},
+        {'title':"厕所","year":"2020"},
+        {'title':"时间","year":"1333"},
+        {'title':"三你那","year":"1666"},
+        {'title':"帝霸","year":"1522"},
+        {'title':"至尊","year":"1000"},
+    ]
 
-def index(name):
-    return "<h1>欢迎进入地狱 %s</h1>"%name
+    return render_template('index.html',name=name,movies=movies)
+
+    
