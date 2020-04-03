2ffa4e20f0f1b10be77c04e13ebdd4fd5c2b9943
Zuiru <2425277916@qq.com>
2020-04-03 11:41:00 +0800

    数据库增加

--- day/watchlist/app.py
+++ day/watchlist/app.py
@@ -1,15 +1,45 @@
+import os,sys
+
 from flask import Flask,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = "sqlite:///"
+else:
+    prefix = "sqlite:////"
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# models  数据层
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+     id = db.Column(db.Integer,primary_key=True)
+     title = db.Column(db.String(60))
+     year = db.Column(db.String(4))
+
+
+
+
+
+
+# views 视图函数
 @app.route('/')
 def index():
     name = "Dom"
     movies = [
         {'title':"大赢家","year":"2020"},
-        {'title':"主女","year":"2020"},
-        {'title':"狗女","year":"2020"},
-        {'title':"猫女","year":"2020"},
-        {'title':"厕所","year":"2020"},
+        {'title':"主2","year":"2020"},
+        {'title':"12","year":"2020"},
+        {'title':"猫3","year":"2020"},
+        {'title':"99所","year":"2020"},
         {'title':"时间","year":"1333"},
         {'title':"三你那","year":"1666"},
         {'title':"帝霸","year":"1522"},
@@ -18,4 +48,16 @@ def index():
 
     return render_template('index.html',name=name,movies=movies)
 
-    
+# 自定义命令
+@app.cli.command()  # 注册命令
+@click.option('--drop',is_flag=True,help='先删除在创建')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据')
+
+
+
+
+
