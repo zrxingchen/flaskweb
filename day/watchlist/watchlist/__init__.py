221377d660b53d4d952598bd1ed4acc5095ab247
Zuiru <2425277916@qq.com>
2020-04-08 17:17:46 +0800

    修改代码及整里错误

--- day/watchlist/watchlist/__init__.py
+++ day/watchlist/watchlist/__init__.py
@@ -15,7 +15,7 @@ else:
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '1903_dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
 
 
 db = SQLAlchemy(app)
