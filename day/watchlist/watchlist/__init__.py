97308a56560b5bdf92eadc35454807950dc851c8
Zuiru <2425277916@qq.com>
2020-04-08 18:26:54 +0800

    部分代码进行修改完善

--- day/watchlist/watchlist/__init__.py
+++ day/watchlist/watchlist/__init__.py
@@ -13,7 +13,7 @@ if WIN:
 else:
     prefix = "sqlite:////"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
 
